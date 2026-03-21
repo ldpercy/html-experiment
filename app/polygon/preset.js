ad1bc5c8ec145bfe51002f488b1f25e0ed05ece1
Paul Dale <ldpercy@gmail.com>
2026-03-22 10:22:29 +1100

    Add potential tri-heptadecagram preset

---
+++ app/polygon/preset.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+const triHeptadecagram = {
+
+	polygon : {
+		sides: 17,
+		pointStep: 8,
+		startDivision: 2,
+		radius: 1100,
+		copies: 3,
+		copyOffset: 4,
+		copyPaths: 'separate',
+		coordinates: 'absolute',
+		decimalPlaces: 0,
+	},
+	style : {
+		startColour: 'forestgreen',
+		fillOpacity: '0.5',
+		fullRule: 'evenodd',
+		antCrawl: true,
+		grid: false,
+		markers: false,
+	},
+
+};
